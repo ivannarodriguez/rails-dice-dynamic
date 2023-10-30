910eaeb819b4369f6eccb2f95eaeb033e43ba6cd
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-30 02:24:25 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get("/", {:controller : => "dice", :action : => "backhome"})
-  get("/dice/2/6", {:controller : => "dice", :action : => "rollstatic"})
-  get("/dice/2/10", {:controller : => "dice", :action : => "rollstatic"})
-  get("/dice/1/20", {:controller : => "dice", :action : => "rollstatic"})
-  get("/dice/5/4", {:controller : => "dice", :action : => "rollstatic"})
-  get("/dice/:number_of_dice/:how_many_sides", {:controller : => "dice", :action : => "rolldynamic"})
+  get("/", {:controller => "dice", :action => "backhome"})
+  get("/dice/2/6", {:controller => "dice", :action => "rollstatic"})
+  get("/dice/2/10", {:controller => "dice", :action => "rollstatic"})
+  get("/dice/1/20", {:controller => "dice", :action => "rollstatic"})
+  get("/dice/5/4", {:controller => "dice", :action => "rollstatic"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "rolldynamic"})
 end
